222e45bcbda5308fb3823173218f714184da5237
Gabe Douda <gabedouda@gmail.com>
2018-11-06 16:59:05 -0700

    drawing of player boards, frames 1-9

---
+++ scripts/script.js
@@ -0,0 +1,43 @@
+function startGame() {
+    var numOfPlayers = document.getElementById("players").value;
+    document.getElementById("startContainer").style.display = "none";
+
+    drawBoards(numOfPlayers);
+}
+
+function drawBoards(numOfPlayers) {
+    var scoreboardContainer = document.getElementById("scoreboardContainer");
+
+    // Using 1 base arrays to keep it consistent with user input
+    for(playerNumber = 1; playerNumber <= numOfPlayers; playerNumber++) {
+        var scoreboard = document.createElement("div");
+
+        var nameLabel = document.createElement("div");
+        nameLabel.classList.add("nameLabel");
+
+        scoreboard.appendChild(nameLabel);
+
+        // Draw frames 1-9
+        for (i = 1; i <= 9; i++) {
+            var frame = document.createElement("div");
+            frame.classList.add("frame");
+            
+            var throwOne = document.createElement("div");
+            throwOne.classList.add("throwOne");
+            frame.appendChild(throwOne);
+            
+            var throwTwo = document.createElement("div");
+            throwTwo.classList.add("throwTwo");
+            frame.appendChild(throwTwo);
+            
+            scoreboard.appendChild(frame);
+        }
+        // Draw frame 10
+        var frame = document.createElement("div");
+        frame.classList.add("frame");
+        scoreboard.appendChild(frame);
+        
+        scoreboardContainer.appendChild(scoreboard);
+    }
+
+}
